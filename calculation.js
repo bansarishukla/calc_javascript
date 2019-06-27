e4284e2c45128b17aa22062bdf6a8a19149639e1
bansari.shukla1996@gmail.com <bansari.shukla1996@gmail.com>
2019-06-27 15:30:10 +0530

    commited

---
+++ calculation.js
@@ -0,0 +1,58 @@
+var exp = '', number, decimal, equal, operator, allowSR= true;
+var textview = document.forms['myForm']['textview'];
+
+function insertNum(num) {
+  if (equal) {
+    exp = num;
+    textview.value = exp;
+    number = true;
+    equal = false;
+  }
+  else {
+    exp = textview.value + num;
+    textview.value = exp;
+    number = true;
+  }
+  if (operator) decimal = false;
+  SR('a');
+}
+function insertOp(op) {
+  textview.value = exp + op;
+  operator = true;
+  equal = false;
+  allowSR = false;
+  SR('a');
+}
+function insertDec(){
+  if (number && !decimal) {
+    textview.value = exp + '.';
+    decimal = true;
+    operator = false;
+  }
+}
+function equalTo() {
+  if(exp) {
+    exp = eval(exp);
+    textview.value = exp;
+    equal = true;
+    decimal = false;
+    number = false;
+    allowSP = true;
+    SR('a');
+  }
+}
+function clean() {
+  exp = '';
+  textview.value = exp;
+  decimal = false;
+}
+function back() {
+  exp = textview.value;
+  exp = exp.substring(0, exp.length - 1);
+  textview.value = exp;
+}
+function square() {
+  let ans = exp * exp;  
+  textview.value = ans;
+
+}
